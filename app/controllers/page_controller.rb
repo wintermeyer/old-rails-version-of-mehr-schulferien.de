447b44a43986d2d02697c1b9cf3ec461ba9514e1
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-01-19 22:24:00 -0800

    New index page with next 3 month overview.

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,22 @@
 class PageController < ApplicationController
   def index
+    date = Day.where(value: Date.today).first
+    @month = date.month
+    @year = @month.year
+    @federal_states = FederalState.all.order(:name)
+    @months = [
+               @month, 
+               Day.where(value: Date.today + 1.month).first.month, 
+               Day.where(value: Date.today + 2.month).first.month, 
+              ]
+
+    if cookies[:last_federal_state] && FederalState.where(slug: cookies[:last_federal_state]).any?
+      @federal_state = FederalState.where(slug: cookies[:last_federal_state]).first
+      expires_in 60.minutes, :public => false
+      fresh_when etag: [@year, @month, @federal_state]
+    else
+      @federal_state = FederalState.all.sample
+    end
   end
 
   def about_us
