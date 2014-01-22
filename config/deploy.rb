79069883b0a7da96105a4a656be5606ff7aae819
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-01-21 22:53:18 -0800

    Added UTF-8 encoding header

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,6 @@ set :deploy_to, '/var/www/mehr-schulferien.de'
 # set :keep_releases, 5
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
