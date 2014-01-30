1b391c8b72a260a211861aedfee044ea43a7cd3a
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-01-30 22:11:08 +0100

    Adds a git revision to etag and fragment cache keys.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 MehrSchulferienDe::Application.initialize!
+
+# Create ENV['RAILS_CACHE_ID'] which is added automagically 
+# to all etags and fragment cache keys.
+#
+ENV['RAILS_CACHE_ID'] = `git log --pretty=format:%h -n1`.strip
