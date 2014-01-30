bdf36797d20ceeac0ff241b4d2b68903721a0783
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-01-30 21:41:46 +0100

    Added linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :deploy_to, '/var/www/mehr-schulferien.de'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system db/sandbox}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
