1e1d2758456fd6bf63932559609d1fba611eb164
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-02-04 14:13:05 +0100

    set :linked_files, %w{config/initializers/omniauth.rb}

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ set :deploy_to, '/var/www/mehr-schulferien.de'
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/initializers/omniauth.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
