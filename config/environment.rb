23e396c4228090f721369aa04ca24976ebb959f3
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2014-02-16 11:51:32 +0100

    Moved ENV['RAILS_APP_VERSION'] to constants.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 MehrSchulferienDe::Application.initialize!
-
-# Create ENV['RAILS_CACHE_ID'] which is added automagically 
-# to all etags and fragment cache keys.
-#
-ENV['RAILS_APP_VERSION'] = `git log --pretty=format:%h -n1`.strip
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,9 @@
 # Define the weekend days
 #
 WEEKEND_WDAYS = [6,0]
+
+
+# Create ENV['RAILS_CACHE_ID'] which is added automagically 
+# to all etags and fragment cache keys.
+#
+ENV['RAILS_APP_VERSION'] = `git log --pretty=format:%h -n1`.strip
