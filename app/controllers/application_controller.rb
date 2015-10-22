a0ea0996876cd705ece3cfda599cdbc9bb2e8046
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2015-10-22 19:45:17 +0200

    Improved content of the description meta tag.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_variables, only: [:show, :index]
+
+  def set_variables
+    @years = Year.where(value: (Date.today.year)..Year.maximum(:value)).order(:value).limit(10)
+  end
 end
